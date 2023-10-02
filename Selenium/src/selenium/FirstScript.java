9ff4e37dc30afe037fcd0b85e740887d9902ceaf
Juan Berta <42556410+JuanBerta@users.noreply.github.com>
2023-10-01 21:39:01 -0300

    Added Swing GUI for Course Availability Checking
    In this commit, I introduced a Swing-based graphical user interface (GUI) to the existing Selenium course availability checker. Users can now input the course URL and specify the wait time conveniently through the GUI. The availability status is displayed in pop-up dialog messages, enhancing user experience and accessibility. This update improves the usability of the application and enhances its functionality.

--- Selenium/src/selenium/FirstScript.java
+++ Selenium/src/selenium/FirstScript.java
@@ -1,21 +1,66 @@
 package selenium;
 
+import javax.swing.JFrame;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class FirstScript {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FirstScript extends JFrame {
 	
-	public static void checkCourseAvailability(String courseURL) {
-	    //Setting system properties of ChromeDriver
+	private JTextField urlField;
+    private JTextField waitTimeField;
+
+    public FirstScript() {
+        setTitle("Selenium Course Availability Checker");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 200);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(3, 2));
+
+        JLabel urlLabel = new JLabel("Course URL:");
+        urlField = new JTextField();
+        JLabel waitTimeLabel = new JLabel("Wait Time (seconds):");
+        waitTimeField = new JTextField();
+
+        JButton checkButton = new JButton("Check Availability");
+        checkButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String courseURL = urlField.getText();
+                int waitTimeInSeconds = Integer.parseInt(waitTimeField.getText());
+
+                boolean isAvailable = checkCourseAvailability(courseURL, waitTimeInSeconds);
+                displayResult(isAvailable);
+            }
+        });
+
+        panel.add(urlLabel);
+        panel.add(urlField);
+        panel.add(waitTimeLabel);
+        panel.add(waitTimeField);
+        panel.add(checkButton);
+
+        add(panel);
+    }
+    
+    public static boolean checkCourseAvailability(String courseURL, int waitTimeInSeconds) {
+        // Setting system properties of ChromeDriver
         System.setProperty("webdriver.chrome.driver", "C:\\Apps\\chromedriver.exe");
-        
+
         // Optional: You can configure Chrome options, such as running headless
         ChromeOptions options = new ChromeOptions();
         // options.addArguments("--headless"); // Uncomment this line to run headless (without a visible browser window)
-        
+
         WebDriver driver = new ChromeDriver(options);
 
         try {
@@ -25,29 +70,39 @@ public class FirstScript {
             // Your automation logic for checking enrollment availability goes here
             // For example, you can look for an enrollment button or check for specific elements on the page
 
-            // Wait 2 seconds
-            Thread.sleep(2000);
-            
+            // Wait for the specified time (in seconds)
+            Thread.sleep(waitTimeInSeconds * 1000);
+
             // For demonstration purposes, let's assume we're looking for an "Enroll" button
             WebElement enrollButton = driver.findElement(By.className("rc-StartDateButton"));
 
             // Check if the enrollment button is visible
             if (enrollButton.isDisplayed()) {
                 System.out.println("Course available: " + courseURL);
+                return true;
             } else {
                 System.out.println("Course not available: " + courseURL);
+                return false;
             }
         } catch (Exception e) {
             System.err.println("Error: " + e.getMessage());
         } finally {
             driver.quit();
         }
+        return false;
+    }
+    
+    public static void displayResult(boolean isAvailable) {
+        String message = isAvailable ? "Course is available!" : "Course is not available.";
+        JOptionPane.showMessageDialog(null, message, "Availability Status", isAvailable ? JOptionPane.INFORMATION_MESSAGE : JOptionPane.ERROR_MESSAGE);
     }
 
     public static void main(String[] args) {
-        // Example usage:
-        String courseURL = "https://www.coursera.org/programs/iec-general-z5sjb/learn/codio-software-testing-for-developers?source=search";
-        checkCourseAvailability(courseURL);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FirstScript().setVisible(true);
+            }
+        });
     }
-	
 }
